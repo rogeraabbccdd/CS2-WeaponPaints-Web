62d28a3d519e4f33f6f542b0f2707e8855b89b14
Kento <rogeraabbccdd@gmail.com>
2026-04-12 22:57:50 +0800

    Move layout to app.js

--- js/app.js
+++ js/app.js
@@ -50,7 +50,100 @@ const app = createApp({
       session,
       page
     }
-  }
+  },
+  template: /*html*/
+    `
+    <v-app>
+      <!-- Navbar -->
+      <v-app-bar color="primary">
+        <v-container class="d-flex align-center">
+          <v-app-bar-title>CS2 Weapon Paints</v-app-bar-title>
+          <v-spacer></v-spacer>
+          <template v-if="session.user.steamid">
+            <v-avatar>
+              <v-img :src="session.user.steam_avatar"></v-img>
+            </v-avatar>
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
+              </template>
+              <v-list>
+                <v-list-item href="./logout.php">
+                  <v-list-item-title>Logout</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </template>
+          <template v-else>
+            <a href="./login.php"><img src='https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png'></a>
+          </template>
+        </v-container>
+      </v-app-bar>
+      <!-- Sidebar -->
+      <v-navigation-drawer v-if="session.user.steamid.length !== 0">
+        <v-list density="compact" nav v-model:selected="page">
+          <v-list-item prepend-icon="mdi-knife-military" title="Knifes" value="knifes"></v-list-item>
+          <v-list-item prepend-icon="mdi-palette" title="Skins" value="skins"></v-list-item>
+          <v-list-item prepend-icon="mdi-boxing-glove" title="Gloves" value="gloves"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="Agents" value="agents"></v-list-item>
+          <v-list-item prepend-icon="mdi-music" title="Musics" value="musics"></v-list-item>
+          <v-list-item prepend-icon="mdi-police-badge" title="Pins" value="pins"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main class="mt-5" v-if="!loaded">
+        <h1 class="text-center">
+          <v-progress-circular color="primary" :size="75" indeterminate class="my-5"></v-progress-circular>
+          <br>
+          Loading...
+        </h1>
+      </v-main>
+      <v-main class="mt-5" v-else-if="session.user.steamid.length === 0">
+        <h1 class="text-center">Please login first.</h1>
+      </v-main>
+      <template v-else>
+        <v-main class="mt-5">
+          <!-- Knifes Page -->
+          <page-knives v-if="page[0] === 'knifes'"></page-knives>
+          <!-- Skins Page -->
+          <page-skins v-if="page[0] === 'skins'"></page-skins>
+          <!-- Gloves Page -->
+          <page-gloves v-if="page[0] === 'gloves'"></page-gloves>
+          <!-- Agents Page -->
+          <page-agents v-if="page[0] === 'agents'"></page-agents>
+          <!-- Music Kits Page -->
+          <page-musics v-if="page[0] === 'musics'"></page-musics>
+          <!-- Pins Page -->
+          <page-pins v-if="page[0] === 'pins'"></page-pins>
+        </v-main>
+        <v-bottom-navigation v-if="mobile" :model-value="page[0]" @update:model-value="v => page[0] = v">
+          <v-btn value="knifes">
+            <v-icon>mdi-knife-military</v-icon>
+            <span>Knifes</span>
+          </v-btn>
+          <v-btn value="skins">
+            <v-icon>mdi-palette</v-icon>
+            <span>Skins</span>
+          </v-btn>
+          <v-btn value="gloves">
+            <v-icon>mdi-boxing-glove</v-icon>
+            <span>Gloves</span>
+          </v-btn>
+          <v-btn value="agents">
+            <v-icon>mdi-account</v-icon>
+            <span>Agents</span>
+          </v-btn>
+          <v-btn value="musics">
+            <v-icon>mdi-music</v-icon>
+            <span>Musics</span>
+          </v-btn>
+          <v-btn value="pins">
+            <v-icon>mdi-police-badge</v-icon>
+            <span>Pins</span>
+          </v-btn>
+        </v-bottom-navigation>
+      </template>
+    </v-app>
+    `
 })
 app.use(vuetify)
 app.component('page-knives', pageKnives)
